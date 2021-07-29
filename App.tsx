648457feba9afb347557fecc828d76696ce08703
lmanfioletti <lucas.manfioletti@gmail.com>
2021-07-29 01:06:44 -0300

    init gameplay

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello word, NLW Together</Text>
+    </View>
+
+
+  );
+  
+}
