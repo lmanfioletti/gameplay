fe8faf608b7d2bf1efe7c7b1377df280770d9e26
lmanfioletti <lucas.manfioletti@gmail.com>
2021-08-02 19:39:17 -0300

    commit_2.1
    Criação de gradiente, e estilização da tela de singin, com instalação de fontes

--- src/screeens/SignIn/index.tsx
+++ src/screeens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View, Image, StatusBar } from 'react-native';
+import {Text, View, Image } from 'react-native';
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImg from '../../assets/illustration.png'
@@ -12,21 +12,15 @@ export function SignIn() {
 
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
 
       <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"
-      
       />
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`} 
-          facilmente
+          Conect-se {`\n`}
+          e organize suas {`\n`} 
+          jogatinas
         </Text>
         <Text style={styles.subtitle}>
           Crie grupos para jogar seus games {`\n`}
